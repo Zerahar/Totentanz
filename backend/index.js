095b2a449972493994dd8cd1b1865de8aed85365
Zerahar <heinisaastamoinen@gmail.com>
2021-03-05 14:25:48 +0200

    Database connection works

--- backend/index.js
+++ backend/index.js
@@ -9,3 +9,18 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+var MongoClient = require('mongodb').MongoClient
+const pass = "dbpass123"
+
+MongoClient.connect(`mongodb+srv://dbAdmin:${pass}@oppari.q4dhm.mongodb.net/data?retryWrites=true&w=majority`, function (err, client) {
+  if (err) throw err
+
+  var db = client.db('data')
+
+  db.collection('characters').find().toArray(function (err, result) {
+    if (err) throw err
+
+    console.log(result)
+  })
+})
