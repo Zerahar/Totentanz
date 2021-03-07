2b975886c08bfc72160cdd25c90f5a5101a62f38
Zerahar <heinisaastamoinen@gmail.com>
2021-03-07 16:44:54 +0200

    Basics for db queries

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3002
+const pass = "dbpass123"
+// var expressMongoDb = require('express-mongo-db');
+// app.use(expressMongoDb(`mongodb+srv://dbAdmin:${pass}@oppari.q4dhm.mongodb.net/data?retryWrites=true&w=majority`));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -10,17 +13,40 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-var MongoClient = require('mongodb').MongoClient
-const pass = "dbpass123"
+var mongo = require('mongodb')
 
-MongoClient.connect(`mongodb+srv://dbAdmin:${pass}@oppari.q4dhm.mongodb.net/data?retryWrites=true&w=majority`, function (err, client) {
+mongo.MongoClient.connect(`mongodb+srv://dbAdmin:${pass}@oppari.q4dhm.mongodb.net/data?retryWrites=true&w=majority`, function (err, client) {
   if (err) throw err
 
-  var db = client.db('data')
-
-  db.collection('characters').find().toArray(function (err, result) {
-    if (err) throw err
-
-    console.log(result)
+  const db = client.db('data')
+// Fetch character
+  app.get('/character/:charId', (req, res) => {
+    const query = { _id: new mongo.ObjectId(req.params.charId)}
+    db.collection('characters').findOne(query, function (err, result) {
+      if (err) throw err
+      res.send(result)
+      db.close
+    })
   })
+
+  // Update character
+  app.post('/character/:charId', (req, res) => {
+    const query = { _id: new mongo.ObjectId(req.params.charId)}
+    const document = {}
+    db.collection('characters').updateOne(query, document, function (err, result) {
+      if (err) throw err
+      res.send(result)
+      db.close
+    })
+  })
+
+    // Add character
+    app.post('/character', (req, res) => {
+      const document = {}
+      db.collection('characters').insertOne(document, function (err, result) {
+        if (err) throw err
+        res.send(result)
+        db.close
+      })
+    })
 })
